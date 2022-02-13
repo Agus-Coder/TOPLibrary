e089f7cad337ca427c6b4ca7c5034df8b5d681d7
Agus-Coder <93748858+Agus-Coder@users.noreply.github.com>
2022-02-13 13:54:09 -0300

    Se añade primera funcionalidad en JS para poder ingresar libros sobre article

--- js-file.js
+++ js-file.js
@@ -1,9 +1,31 @@
-let myLibrary = [];
 
-function book() {
+const collection = document.querySelector('#collection')
+let newBook = {}
 
+function book(name,author, year) {
+    this.name = name
+    this.author = author
+    this.year = year
 }
 
-function addBookToLibrary(){
-    
+
+
+function display(){
+    console.log('funciona')
+
+    newBook = new book(
+        document.getElementById('title').value,
+        document.getElementById('author').value,
+        document.getElementById('year').value
+    )
+
+    const div = document.createElement('div');
+    collection.appendChild(div);
+    div.textContent = ` El libro ingresado es ${newBook.name}, 
+    su autor es ${newBook.author} y el anio de publicacion es
+    ${newBook.year}`
 }
+
+const button = document.querySelector('#submitButton')
+button.addEventListener('click', display)
+
